277b78ace53687cd376fbdcf2785c2464f4c121d
Riley LePrell <150089004+RileyLePrell@users.noreply.github.com>
2023-11-30 14:29:29 -0500

    Update test.py

--- test.py
+++ test.py
@@ -2,10 +2,13 @@ import streamlit as st
 import pandas as pd
 import plotly.graph_objs as go
 
-# Directly load the data
+# Set the page layout to wide
+st.set_page_config(layout="wide")
+
+# Load the data
 df = pd.read_csv('ej_nc.csv')
 
-# Define the RPL_EJI categories
+# Define the RPL_EJI categories and their corresponding numerical ranges
 eji_categories = {
     'Low': (0, 0.25),
     'Low/Moderate': (0.25, 0.5),
@@ -13,35 +16,16 @@ eji_categories = {
     'High': (0.75, 1)
 }
 
+# Function to calculate percentages for each demographic within a county
+def calculate_demographics(data):
+    sums = data[['white', 'black', 'asian', 'latino', 'other']].sum()
+    total = sums.sum()
+    return (sums / total * 100).to_dict()
+
 # Function to calculate percentages for each demographic within each EJI category in a county
 def calculate_eji_demographics(data, eji_range):
     filtered_data = data[(data['RPL_EJI'] >= eji_range[0]) & (data['RPL_EJI'] < eji_range[1])]
-    sums = filtered_data[['white', 'black', 'asian', 'latino', 'other']].sum()
-    total = sums.sum()
-    percentages = {cat: sums[cat] / total * 100 for cat in ['white', 'black', 'asian', 'latino', 'other']}
-    return percentages, total
-
-# Sum the demographic data for each county
-county_totals = df.groupby('COUNTY')[['white', 'black', 'asian', 'latino', 'other']].sum().reset_index()
-
-# Calculate percentages for each demographic within each county
-for category in ['white', 'black', 'asian', 'latino', 'other']:
-    county_totals[f'percent_{category}'] = county_totals[category] / county_totals[['white', 'black', 'asian', 'latino', 'other']].sum(axis=1) * 100
-
-# Get unique county names
-county_names = county_totals['COUNTY'].unique()
-
-# Sidebar for county selection
-selected_county = st.sidebar.selectbox('Select a COUNTY', county_names)
-
-# Filter data for selected county
-county_data = df[df['COUNTY'] == selected_county]
-
-# Sidebar for EJI category selection
-selected_eji_category = st.sidebar.selectbox('Select an EJI Category', list(eji_categories.keys()))
-
-# Calculate demographics for the selected EJI category within the selected county
-eji_demographics, eji_total = calculate_eji_demographics(county_data, eji_categories[selected_eji_category])
+    return calculate_demographics(filtered_data)
 
 # Custom colors for each demographic
 colors = {
@@ -52,132 +36,87 @@ colors = {
     'other': '#43a8b5'
 }
 
-# Plotting function for the county demographics bar chart
-def create_demographic_plotly_bar(data, county_name):
+# Plotting function for demographic data
+def create_demographic_bar_chart(data, title, legend_labels, display_legend=False):
     fig = go.Figure()
-    categories = ['white', 'black', 'asian', 'latino', 'other']
-    color_values = [colors[cat] for cat in categories]
-
-    # Add bars for each demographic
-    for i, category in enumerate(categories):
+    annotations = []
+    for i, (category, percentage) in enumerate(data.items()):
         fig.add_trace(go.Bar(
-            x=[data[f'percent_{category}'].iloc[0]],
-            y=[county_name],
-            name=category.capitalize(),
+            x=[percentage],
+            y=['Demographics'],
+            name=legend_labels[category],
             orientation='h',
-            marker=dict(
-                color=color_values[i],
-                line=dict(color='white', width=0)  # No border
-            ),
-            hoverinfo='text',
-            text=f"{category.capitalize()}: {data[f'percent_{category}'].iloc[0]:.1f}%"
+            marker=dict(color=colors[category]),
+            text='',
+            hoverinfo='none'
         ))
-
-    # Update layout for a stacked bar chart
-    fig.update_layout(
-        barmode='stack',
-        title={
-            'text': f"<b>{county_name}</b>",
-            'y':0.95,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top',
-        },
-        title_font_size=22,
-        height=300,  # Height of the bar
-        margin=dict(l=20, r=20, t=100, b=100),  # Adjust margins to fit the legend below
-        showlegend=True,
-        legend=dict(
-            orientation="h",
-            yanchor="bottom",
-            y=-0.15,  # Position the legend between the title and the bar
-            xanchor="center",
-            x=0.5
-        ),
-        xaxis=dict(
-            showgrid=False,
-            zeroline=False,
-            showticklabels=False,
-        ),
-        yaxis=dict(
-            showgrid=False,
-            zeroline=False,
-            showticklabels=False,
-        )
-    )
-
-    # Disable the plotly toolbar
-    config = {"displayModeBar": False}
-
-    return fig, config
-
-# Plotting function for the EJI demographics bar chart
-def create_eji_demographic_bar(demographics, total, county_name, eji_category):
-    fig = go.Figure()
-    categories = ['white', 'black', 'asian', 'latino', 'other']
-    color_values = [colors[cat] for cat in categories]
+        # Add annotations below the bar
+        annotations.append(dict(
+            x=sum(list(data.values())[:i+1]) - (percentage/2),  # Position at the center of each segment
+            y=-.5,  # Slightly below the bar
+            text=f"{percentage:.1f}%",
+            showarrow=False,
+            font=dict(color="Black")
+        ))
     
-    # Add bars for each demographic
-    for i, category in enumerate(categories):
-        # Calculate the value for each category
-        value = demographics.get(category, 0) / 100 * total
-        fig.add_trace(go.Bar(
-            x=[value],
-            y=[f"{eji_category} EJI"],
-            name=category.capitalize(),
-            orientation='h',
-            marker=dict(
-                color=color_values[i],
-                line=dict(color='white', width=0)  # No border
-            ),
-            hoverinfo='text',
-            text=f"{category.capitalize()}: {demographics.get(category, 0):.1f}%"
-        ))
-
-    # Update layout for a stacked bar chart
     fig.update_layout(
         barmode='stack',
-        title={
-            'text': f"<b>{county_name} - {eji_category} EJI Demographics</b>",
-            'y':0.95,
-            'x':0.5,
-            'xanchor': 'center',
-            'yanchor': 'top',
-        },
-        title_font_size=22,
-        height=300,  # Height of the bar
-        margin=dict(l=20, r=20, t=100, b=100),  # Adjust margins to fit the legend below
-        showlegend=True,
-        legend=dict(
-            orientation="h",
-            yanchor="bottom",
-            y=-0.15,  # Position the legend between the title and the bar
-            xanchor="center",
-            x=0.5
-        ),
-        xaxis=dict(
-            showgrid=False,
-            zeroline=False,
-            showticklabels=False,
-        ),
-        yaxis=dict(
-            showgrid=False,
-            zeroline=False,
-            showticklabels=False,
-        )
+        title=title,
+        title_x=0.5,
+        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False, range=[0, 100]),
+        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+        annotations=annotations,
+        showlegend=display_legend,
+        legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="center", x=0.5),
+        height=300  # Fixed height for a 1:5 aspect ratio
     )
+    return fig
 
-    # Disable the plotly toolbar
-    config = {"displayModeBar": False}
+# Get unique county names for dropdown
+county_info = df[['COUNTY', 'StateAbbr']].drop_duplicates()
 
-    return fig, config
+# Sidebar for county selection
+selected_county_info = st.sidebar.selectbox('Select a COUNTY', county_info.itertuples(index=False), format_func=lambda x: x.COUNTY)
+selected_county, selected_state_abbr = selected_county_info.COUNTY, selected_county_info.StateAbbr
 
-# Display the title and the first bar chart for overall county demographics
-st.title(f"Demographic Distribution for {selected_county}")
-plotly_fig, config = create_demographic_plotly_bar(county_totals[county_totals['COUNTY'] == selected_county], selected_county)
-st.plotly_chart(plotly_fig, use_container_width=True, config=config)
+# Sidebar for EJI category selection
+selected_eji_category = st.sidebar.selectbox('Select an EJI Category', list(eji_categories.keys()))
 
-# Display the title and the second bar chart for demographics based on EJI category
-st.title(f"{selected_county} - {selected_eji_category} EJI Demographics")
-eji_plotly_fig, eji_config = create_eji_demographic_bar(eji_demographics, eji_total, selected_county, selected_eji_category)
-st.plotly_chart(eji_plotly_fig, use_container_width=True, config=eji_config)
+# Filter data for selected county
+county_data = df[df['COUNTY'] == selected_county]
+
+# Calculate overall demographics for the selected county
+overall_demographics = calculate_demographics(county_data)
+
+# Calculate demographics for the selected EJI category within the selected county
+eji_demographics = calculate_eji_demographics(county_data, eji_categories[selected_eji_category])
+
+# Set legend labels for the bar charts
+legend_labels = {'white': 'White', 'black': 'Black', 'latino': 'Latino', 'asian': 'Asian', 'other': 'Other'}
+
+# Display the main title centered at the top using HTML and CSS
+st.markdown(f"<h1 style='text-align: center;'>{selected_county}, {selected_state_abbr}</h1>", unsafe_allow_html=True)
+
+# Display the legend directly below the main title using similar HTML and CSS styling
+legend_html = "<div style='text-align: center; margin-bottom: 20px;'>"
+for category in colors:
+    legend_html += f"<span style='display: inline-block; margin-right: 10px;'>"
+    legend_html += f"<span style='background: {colors[category]}; width: 15px; height: 15px; display: inline-block;'></span>"
+    legend_html += f" {legend_labels[category]}</span>"
+legend_html += "</div>"
+st.markdown(legend_html, unsafe_allow_html=True)
+
+# Create a two-column layout for side-by-side bar charts
+col1, col2 = st.columns(2)
+
+with col1:
+    # Use Markdown with unsafe_allow_html to allow HTML content
+    st.markdown("<h3 style='text-align: center;'>Alamance Low EJ Risk</h3>", unsafe_allow_html=True)
+    eji_fig = create_demographic_bar_chart(eji_demographics, '', legend_labels, display_legend=False)
+    st.plotly_chart(eji_fig, use_container_width=True, config={'displayModeBar': False})
+
+with col2:
+    # Use Markdown with unsafe_allow_html to allow HTML content
+    st.markdown("<h3 style='text-align: center;'>Alamance Surrounding Area</h3>", unsafe_allow_html=True)
+    overall_fig = create_demographic_bar_chart(overall_demographics, '', legend_labels)
+    st.plotly_chart(overall_fig, use_container_width=True, config={'displayModeBar': False})
